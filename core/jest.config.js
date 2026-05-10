ea3e0ad011b49fb76e1187b366bd9f8a9fc5812e
ramiro martinez <ramiro.md90@gmail.com>
2026-05-09 22:44:46 -0300

    Jest for Unit testing

---
+++ core/jest.config.js
@@ -0,0 +1,18 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  preset: 'ts-jest',
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  roots: ["<rootDir>/tests"],
+  testMatch: ["**/*.test.ts"],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  clearMocks: true,
+};
